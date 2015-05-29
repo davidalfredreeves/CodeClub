c399443f4e8e4f0ef207d1ee9a4c18309b66a60c
davidalfredreeves <david.alfred.reeves@gmail.com>
2015-05-29 13:51:34 +0100

    problem set #4
    first attempt

---
+++ Problem Set 4/functions.py
@@ -0,0 +1,64 @@
+import datetime
+import random
+from os import walk
+
+def inputint(number):
+    while True:
+        try:
+            n = number
+            if n == float(n):
+                return("Not a valid integer! Please try again.")
+            n = int(n)
+            break
+        except ValueError:
+            return("Not a valid integer! Please try again.")
+    return("Great, you successfully entered an integer!")
+
+
+def log(message):
+    l = open("log.txt", "a")
+    l.write(message)
+    l.write("\n")
+    l.close()
+    return("You've entered " + "'" + message + "'" + " to the log.")
+
+def daysold(date):
+    try:
+        now = datetime.datetime.now()
+        newNow = now.date()
+        then = datetime.datetime.strptime(date, "%d/%m/%Y")
+        diff = newNow - then.date()
+        return("There have been " + str(diff.days) + " days since " + date + ".")
+    except TypeError:
+        return("False.")
+
+def parse_date(entry):
+    try:
+        date = datetime.datetime.strptime(str(entry), "%d/%m/%Y")
+        newDate = date.strftime("%d/%m/%Y")
+        return(newDate)
+    except ValueError:
+        return("Please enter a date in the dd/mm/yyyy format, and as a string.")
+
+def randomSample(aList, size):
+    if size > len(aList):
+        return("The size of sample you have requested is greater than the list")
+    sample = random.randint(1, size)
+    count = size
+    newList = []
+    while count > 0:
+        number = random.randint(0, len(aList)-1)
+        newList.append(aList[number])
+        aList.remove(aList[number])
+        count = count - 1
+    return(newList)
+
+def get_files(path):
+    files = []
+    for (dirpath, dirnames, filenames) in walk(path):
+         files.extend(filenames)
+         break
+    return(files)
+
+
+
---
+++ Problem Set 4/test_functions.py
@@ -0,0 +1,31 @@
+import functions
+
+print(functions.inputint(4))
+print("EXPECTED: 'Great, you succesfully entered an integer.'")
+print(functions.inputint("4"))
+print("EXPECTED: 'Great, you succesfully entered an integer.'")
+print(functions.inputint(4.1))
+print("EXPECTED: 'Not a valid integer! Please try again.'")
+print(functions.inputint("fish"))
+print("EXPECTED: 'Not a valid integer! Please try again.'")
+
+print(functions.log("Test1"))
+print("EXPECTED: 'You've entered 'Test1' to the log.'")
+
+print(functions.daysold(28/5/15))
+print("EXPECTED: 'False'")
+print(functions.daysold("28/05/2015"))
+print("EXPECTED: 'There have been 1 days since 28/5/15.'")
+print(functions.daysold(28515))
+print("EXPECTED: 'False'")
+
+print(functions.parse_date("17/10/1987"))
+print("EXPECTED: '17/10/1987'")
+print(functions.parse_date(17/10/1987))
+print("EXPECTED: 'Please enter a date in the dd/mm/yyyy format, and as a string.")
+print(functions.parse_date(1255423525))
+print("EXPECTED: 'Please enter a date in the dd/mm/yyyy format, and as a string.")
+
+print(functions.randomSample(["1", "2", "3", "4", "5"], 3))
+
+print(functions.get_files("testFolder"))
